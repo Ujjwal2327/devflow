49e9c9a67ab65fe9fd6827e5d65ebfb1527b0af4
Ujjwal Maheshwari <ujjwal2327@gmail.com>
2024-03-10 04:33:51 +0530

    update image url

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -52,7 +52,7 @@ const QuestionCard = ({
       {/* metrices */}
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          imgUrl="/assets/icons/avatar.svg"
+          imgUrl={author.picture}
           alt="author"
           value={author.name}
           title={`- asked ${getTimeStamp(createdAt)}`}
